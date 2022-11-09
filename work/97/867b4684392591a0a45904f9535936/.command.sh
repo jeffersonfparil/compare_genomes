f4d06f507d8be5531e38d9710d232365a8d3d241
Ubuntu <ubuntu@weedomics-vm-3.novalocal>
2022-11-09 06:28:02 +0000

    fresh test setup.nf, next will be orthofinder.nf, then Cafe5

--- work/97/867b4684392591a0a45904f9535936/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd /data-weedomics-3
-echo 'using Pkg; Pkg.add(["Plots", "DataFrames", "CSV", "ProgressMeter", "JLD2"])' > install_julia_packages.jl
-julia install_julia_packages.jl
-rm install_julia_packages.jl
